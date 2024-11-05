77c1736ceaba62f296bd22f1ddf0177d3b752ebb
Valentino Rossi <valerossi2004@gmail.com>
2024-11-05 14:42:05 -0300

    appointments view first version

--- lib/presentation/views/clients/appointments_view.dart
+++ lib/presentation/views/clients/appointments_view.dart
@@ -1,15 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:spa_app/domain/entities/appointment.dart';
+import 'package:spa_app/presentation/providers/providers.dart';
+import 'package:spa_app/presentation/widgets/widgets.dart';
 
-class AppointmentsView extends StatelessWidget {
+class AppointmentsView extends ConsumerStatefulWidget {
   const AppointmentsView({super.key});
 
   @override
+  AppointmentsViewState createState() => AppointmentsViewState();
+}
+
+class AppointmentsViewState extends ConsumerState<AppointmentsView> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(clientAppointmentsProvider.notifier).loadAppointments();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Turnos'),
-      ),
-      body: const Center(child: Text('Turnos pendientes')),
+
+    final textStyles = Theme.of(context).textTheme;
+
+    final clientAppointments = ref.watch(clientAppointmentsProvider);
+    final List<Appointment> upComingAppointments= [];
+    final List<Appointment> previousAppointments= [];
+
+    clientAppointments.forEach((element) {
+      if (element.date.isAfter(DateTime.now())) {
+        upComingAppointments.add(element);
+      }
+    });
+
+    clientAppointments.forEach((element) {
+      if (element.date.isBefore(DateTime.now())) {
+        previousAppointments.add(element);
+      }
+    });
+    
+    return Column(
+      children: [
+        const CustomAppbar(),
+
+        Container(
+          width: double.infinity,
+          margin: const EdgeInsets.all(10),
+          child: Text("Turnos Pendientes",textAlign: TextAlign.start, style: textStyles.titleMedium,),
+        ),
+
+        AppointmentsListView(upComingAppointments: upComingAppointments),
+
+        const Divider(
+          height: 50,
+        ),
+
+        Container(
+          width: double.infinity,
+          margin: const EdgeInsets.only(left: 10, bottom: 20),
+          child: Text("Historial de Turnos",textAlign: TextAlign.start, style: textStyles.titleMedium,),
+        ),
+
+        AppointmentsListView(upComingAppointments: previousAppointments),
+
+      ],
     );
   }
 }
